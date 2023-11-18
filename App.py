a54e8fd1588196f9cb4767b51e577e6b826353c9
Nikolaj Nielsen <nsknielsen@gmail.com>
2023-11-18 10:41:51 +0100

    Replaces seconds 3-5 with silence

--- App.py
+++ App.py
@@ -1,4 +1,5 @@
 import argparse
+from pydub import AudioSegment
 
 def get_file():
     arg_parser = argparse.ArgumentParser()
@@ -8,5 +9,17 @@ def get_file():
 
 if __name__ == "__main__":
     file = get_file()
-
     
+    track = AudioSegment.from_wav(file)
+
+    start_duration = 3 * 1000
+    before_silence = track[:start_duration]
+
+    silence_duration = 2 * 1000
+    silence = AudioSegment.silent(duration=silence_duration)
+
+    after_silence = track[start_duration + silence_duration:]
+
+    new_track = before_silence + silence + after_silence
+    new_track.export("silenced.wav", format="wav")
+    print("Generated 2 seconds of silence at seconds 3-5 of file: ", file)
