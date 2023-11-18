fb3f5ce66bb5d0516b5022da2356c85c9729317c
Nikolaj Nielsen <nsknielsen@gmail.com>
2023-11-18 11:34:48 +0100

    Adds silences until end of track reached

--- App.py
+++ App.py
@@ -8,34 +8,35 @@ def get_file():
     args = arg_parser.parse_args()
     return args.file
 
-def get_durations(silence_lower=2, silence_upper=5, sound_lower=2, sound_upper=5):
-    silence_duration = random.randint(silence_lower, silence_upper) * 1000
-    sound_duration = random.randint(sound_lower, sound_upper) * 1000
+def get_duration_millis(silence_lower=2000, silence_upper=5000, sound_lower=2000, sound_upper=5000):
+    silence_duration = random.randint(silence_lower, silence_upper)
+    sound_duration = random.randint(sound_lower, sound_upper)
     return (silence_duration, sound_duration)
+
+def will_not_overflow_track_end(track_length, current_new_track_length, silence_duration_millis, sound_duration_millis):
+    next_track_pointer_seconds = current_new_track_length + silence_duration_millis / 1000 + sound_duration_millis / 1000
+    return track_length > next_track_pointer_seconds
 
 def with_silence(track):
     start_duration = 4 * 1000
-    start = track[:start_duration]
+    new_track = track[:start_duration]
 
-    new_track_segments = [start]
     track_pointer = start_duration
 
-  
-    for i in range(5):
-        silence_duration, sound_duration = get_durations()
+    track_length = track.duration_seconds
+
+    silence_duration, sound_duration = get_duration_millis()
+    while (will_not_overflow_track_end(track_length, new_track.duration_seconds, silence_duration, sound_duration)):
         silence_segment = AudioSegment.silent(duration=silence_duration)
-        new_track_segments.append(silence_segment)
+        new_track += silence_segment
         track_pointer += silence_duration
         sound_segment = track[track_pointer:track_pointer + sound_duration]
         track_pointer += sound_duration
-        new_track_segments.append(sound_segment)
+        new_track += sound_segment
+        silence_duration, sound_duration = get_duration_millis()
 
     end = track[track_pointer:]
-    new_track_segments.append(end)
-
-    new_track = AudioSegment.empty()
-    for i in range(len(new_track_segments)):
-        new_track += new_track_segments[i]
+    new_track += end
 
     return new_track
 
