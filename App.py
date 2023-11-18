99b5bf0abbc1e0499881dcb40142982428a5f1b5
Nikolaj Nielsen <nsknielsen@gmail.com>
2023-11-18 14:53:50 +0100

    Silence and sound segment duration bounds are optional arguments

--- App.py
+++ App.py
@@ -3,22 +3,25 @@ from pydub import AudioSegment
 import random
 import os
 
-def get_file():
+def get_args():
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument('-f', '--file')
-    args = arg_parser.parse_args()
-    return args.file
+    arg_parser.add_argument('--silence_lower', default=2000)
+    arg_parser.add_argument('--silence_upper', default=5000)
+    arg_parser.add_argument('--sound_lower', default=2000)
+    arg_parser.add_argument('--sound_upper', default=5000)
+    return arg_parser.parse_args()
 
-def get_duration_millis(silence_lower=2000, silence_upper=5000, sound_lower=2000, sound_upper=5000):
+def get_duration_millis(silence_lower, silence_upper, sound_lower, sound_upper):
     silence_duration = random.randint(silence_lower, silence_upper)
     sound_duration = random.randint(sound_lower, sound_upper)
     return (silence_duration, sound_duration)
 
-def will_not_overflow_track_end(track_length, current_new_track_length, silence_duration_millis, sound_duration_millis):
+def will_not_overflow_track_end(track_length_seconds, current_new_track_length, silence_duration_millis, sound_duration_millis):
     next_track_pointer_seconds = current_new_track_length + silence_duration_millis / 1000 + sound_duration_millis / 1000
-    return track_length > next_track_pointer_seconds
+    return track_length_seconds > next_track_pointer_seconds
 
-def with_silences(track):
+def with_silences(track, silence_lower, silence_upper, sound_lower, sound_upper):
     start_duration = 4 * 1000
     new_track = track[:start_duration]
 
@@ -26,7 +29,7 @@ def with_silences(track):
 
     track_length = track.duration_seconds
 
-    silence_duration, sound_duration = get_duration_millis()
+    silence_duration, sound_duration = get_duration_millis(silence_lower, silence_upper, sound_lower, sound_upper)
     while (will_not_overflow_track_end(track_length, new_track.duration_seconds, silence_duration, sound_duration)):
         silence_segment = AudioSegment.silent(duration=silence_duration)
         new_track += silence_segment
@@ -34,7 +37,7 @@ def with_silences(track):
         sound_segment = track[track_pointer:track_pointer + sound_duration]
         track_pointer += sound_duration
         new_track += sound_segment
-        silence_duration, sound_duration = get_duration_millis()
+        silence_duration, sound_duration = get_duration_millis(silence_lower, silence_upper, sound_lower, sound_upper)
 
     end = track[track_pointer:]
     new_track += end
@@ -42,9 +45,15 @@ def with_silences(track):
     return new_track
 
 if __name__ == "__main__":
-    file = get_file()
+    args = get_args()
+    file = args.file
     track = AudioSegment.from_wav(file)
-    new_track = with_silences(track)
+    silence_lower = args.silence_lower
+    silence_upper = args.silence_upper
+    sound_lower = args.sound_lower
+    sound_upper = args.sound_upper
+    new_track = with_silences(track, silence_lower, silence_upper, sound_lower, sound_upper)
+
     old_filename = os.path.basename(os.path.normpath(file))
     new_filename = f'silenced-{old_filename}.wav'
     new_track.export(new_filename, format="wav")
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 This is a command-line program that requires an argument with a path to a WAV file. 
 The program selects random 2-5 second sequences of this WAV file that are replaced with silence in an output file.
 
-Use cases:
+Usecases:
 - For practicing tempo as a musician. Add silences to a recording and play along with it, making sure to keep tempo during the silenced parts of the track.
 
 ### Prerequisites
@@ -11,11 +11,17 @@ Python 3 and `pip` or a similar package manager.
 ### Usage
 1. Clone this repository.
 2. From the root folder, install dependencies eg. with `pip install -r requirements.txt`.
-3. Run the program with `python App.py --file [absolutePathToYourWavFile]`
+3. Run the program with `python App.py --file [absolutePathToYourWavFile] [optionalArguments]`
 4. The output file is found in the root folder as `silenced-[inputFileName].wav`
 
 ### Arguments
-`-f | --file`: The .wav file that you want to add silences to.
+- `-f | --file`: The .wav file that you want to add silences to.
+- `--silence_lower` (optional): Lower bound for the duration of each silence segment in milliseconds.
+- `--silence_upper` (optional): Upper bound for the duration of each silence segment in milliseconds.
+- `--sound_lower` (optional): Lower bound for the duration of each sound segment in milliseconds.
+- `--sound_upper` (optional): Upper bound for the duration of each sound segment in milliseconds.
+
+<b>Example usage:</b> `python App.py --file "C:\Users\nsknielsen\desktop\aalborg_polka.wav" --silence_lower 3000 --silence_upper 6000`
 
 ## License
 [GNU General Public License](https://www.gnu.org/licenses/gpl-3.0.txt)
